b764cbaed0e4178fc09cde1aea516947ba4459ce
Venugopal <javvajivenugopalrao@gmail.com>
2026-04-27 15:13:48 +0600

    Update seekercallpage.jsx

--- Collageproject/src/components/Pages/SeekerDhashboard/seekercallpage.jsx
+++ Collageproject/src/components/Pages/SeekerDhashboard/seekercallpage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
 import {
-  createCallSession,
+
   getCallToken,
   endCallAPI 
 } from "../../../Service/callService";
