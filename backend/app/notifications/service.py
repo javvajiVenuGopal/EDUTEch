244b6745bad08ec02518fb148b777fadd8bc3dbc
Venugopal <javvajivenugopalrao@gmail.com>
2026-04-29 14:02:51 +0600

    Update service.py

--- backend/app/notifications/service.py
+++ backend/app/notifications/service.py
@@ -1,23 +1,30 @@
 from app.notifications.models import Notification
 from app.notifications.ws_manager import manager
+from app.core.database import SessionLocal
 
 
-def create_notification(db, user_id, title, message):
 
-    notification = Notification(
-        user_id=user_id,
-        title=title,
-        message=message
-    )
+def create_notification(user_id, title, message):
 
-    db.add(notification)
-    db.commit()
+    db = SessionLocal()
 
-    # realtime websocket push
-    manager.send_to_user_sync(
-        user_id,
-        {
-            "title": title,
-            "message": message
-        }
-    )
+    try:
+        notification = Notification(
+            user_id=user_id,
+            title=title,
+            message=message
+        )
+
+        db.add(notification)
+        db.commit()
+
+        manager.send_to_user_sync(
+            user_id,
+            {
+                "title": title,
+                "message": message
+            }
+        )
+
+    finally:
+        db.close()
